d61d5e40f090ef100200fb70309c97aca5126851
yanshaoqiang <shaoqiang_yan@163.com>
2022-04-28 22:46:31 +0800

    init

---
+++ blog-2022/index.md
@@ -0,0 +1,11 @@
+--- 
+title: home
+layout: blog.njk
+---
+
+```shell
+# 运行服务, 将md转为html
+npm run serve
+```
+
+
---
+++ blog-2022/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./**/*.{md,njk,css}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
+}
